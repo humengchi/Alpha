f0b17a82fd6358f3ce73d6c0f32b164efa5a24a2
胡梦驰 <eric.hu@51jinmai.com>
2018-10-25 17:30:35 +0800

    project

---
+++ JenkinsTest/JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by 胡梦驰 on 2018/10/25.
+//  Copyright © 2018年 胡梦驰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsTest/JenkinsTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JenkinsTest
+//
+//  Created by 胡梦驰 on 2018/10/25.
+//  Copyright © 2018年 胡梦驰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
